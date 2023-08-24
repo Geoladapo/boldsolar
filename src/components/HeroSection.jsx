dc556c19bee8f40a5f1199c47e1dc05a19550227
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-24 20:46:45 +0100

    herosection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,15 +2,15 @@ import NavBar from "./NavBar";
 
 
 const HeroSection = () => {
-    return <div className="h-screen  bg-[#0A2640]">
+    return <div className="h-full  bg-[#0A2640]">
         <NavBar />
         <div className="flex flex-col ">
             <div className="flex mt-10">
                 <div className="flex-1 flex flex-col justify-center items-center gap-8">
                     <div className="text-white px-16">
                         <h2 className="font-manrope text-5xl leading-[72px] mb-5">Save time by building  fast with Boldo Template </h2>
-                        <p className="text-base font-normal font-openSans leading-7">Funding handshake buyer business-to-business metrics iPad partnership.<br/>
-                         First mover advantage innovator success deployment non-disclosure.</p>
+                        <p className="text-base font-normal font-openSans leading-7">Funding handshake buyer business-to-business metrics iPad partnership.<br />
+                            First mover advantage innovator success deployment non-disclosure.</p>
                     </div>
                     <div className="flex justify-start items-center ml-32 gap-5 w-full font-openSans">
                         <button className="bg-[#69E6A6] text-[#0A2640] py-4 px-14 font-bold text-xl rounded-full">Buy template</button>
@@ -18,10 +18,17 @@ const HeroSection = () => {
                     </div>
                 </div>
                 <div className="flex-1 flex items-center justify-center">
-            <img className="w-[493.86px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692886443/boldo/hero_graphics_dfka9j.png" alt="hero images" />
+                    <img className="w-[493.86px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692886443/boldo/hero_graphics_dfka9j.png" alt="hero images" />
                 </div>
             </div>
-            <div></div>
+            <div className=" flex gap-10 justify-center items-center mx-auto py-20">
+                <img className="w-[146.51px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692889368/boldo/Logo_2_acefyt.png" alt="boldo" />
+                <img className="w-[146.51px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692889368/boldo/Logo_3_lcy6ta.png" alt="presto" />
+                <img className="w-[146.51px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692889368/boldo/Logo_2_acefyt.png" alt="boldo" />
+                <img className="w-[146.51px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692889368/boldo/Logo_3_lcy6ta.png" alt="presto" />
+                <img className="w-[146.51px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692889368/boldo/Logo_2_acefyt.png" alt="boldo" />
+                <img className="w-[146.51px]" src="https://res.cloudinary.com/dhvwthnzq/image/upload/v1692889368/boldo/Logo_3_lcy6ta.png" alt="presto" />
+            </div>
         </div>
     </div>;
 };
