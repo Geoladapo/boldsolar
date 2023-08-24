2f9f8802aa9b2437225e6659649cfbda3824bb64
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-24 23:58:25 +0100

    mock data

---
+++ src/MockData/data.js
@@ -0,0 +1,23 @@
+export const services = [
+    {
+      id: 1,
+      imgLink: 'https://res.cloudinary.com/dhvwthnzq/image/upload/v1692913506/boldo/Rectangle_1270_md2kvr.png',
+      title:"Cool feature title",
+      description: 'Learning curve network effects return on investment.',
+      buttonLink: '/'
+    },
+    {
+      id: 2,
+      imgLink: 'https://res.cloudinary.com/dhvwthnzq/image/upload/v1692913506/boldo/Rectangle_1270_1_jqa3vw.png',
+      title:"Even cooler feature",
+      description: 'Learning curve network effects return on investment.',
+      buttonLink: '/'
+    },
+    {
+      id: 3,
+      imgLink: 'https://res.cloudinary.com/dhvwthnzq/image/upload/v1692913506/boldo/Rectangle_1270_2_ao3pmw.png',
+      title:"Cool feature title",
+      description: 'Learning curve network effects return on investment.',
+      buttonLink: '/'
+    },
+  ];
