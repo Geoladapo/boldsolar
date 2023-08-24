b40fc02d43ef989f4db8010298e98b79965d7977
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-08-24 20:46:34 +0100

    implemented Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,9 +5,9 @@ const NavBar = () => {
             <img className="w-40" src={"https://res.cloudinary.com/dhvwthnzq/image/upload/v1692882879/boldo/Logo_1_isvimz.png"} alt="Boldo Logo" />
         </div>
         <div className="text-white flex justify-center text-base items-center font-openSans gap-10">
-            <p className="">Products</p>
-            <p>Services</p>
-            <p>About</p>
+            <p className="cursor-pointer">Products</p>
+            <p className="cursor-pointer">Services</p>
+            <p className="cursor-pointer">About</p>
             <button className="bg-white font-bold text-[#0A2640] rounded-3xl px-9 py-2 mr-5">Log In</button>
         </div>
     </div>;
